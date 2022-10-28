0c09a8b45af7834f414dc1a250ba9987e31b0d3f
sgrg007 <sgrg007@gmail.com>
2022-10-28 17:50:26 -0500

    initial commit

--- src/main/java/com/example/demo1/controller/Hello.java
+++ src/main/java/com/example/demo1/controller/Hello.java
@@ -1,17 +1,23 @@
 package com.example.demo1.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class Hello {
     @GetMapping("/hello")
-    public String hello(){
-        return "hello!!";
+    public String hello() {
+        String helloString = "hello!!";
+        log.info("returing: {}", helloString);
+        return helloString;
     }
 
     @GetMapping("/system")
-    public String helloSystem(){
-        return System.getenv().toString();
+    public String helloSystem() {
+        String systemString = System.getenv().toString();
+        log.info("returning: {}", systemString);
+        return systemString;
     }
 }
